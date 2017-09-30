d664d240defb49e72228c4663daa901d116b6ef8
timewz <TimeWz667@gmail.com>
2017-09-30 13:13:28 +0100

    Add abms

--- src/hgm/abmodel/ObserverABM.java
+++ src/hgm/abmodel/ObserverABM.java
@@ -1,8 +1,20 @@
 package hgm.abmodel;
 
+import mcore.*;
+
 /**
  *
  * Created by TimeWz on 2017/6/16.
  */
-public class ObserverABM {
+public class ObserverABM extends AbsObserver<AgentBasedModel> {
+
+    @Override
+    public void initialiseObservation(AgentBasedModel model, double ti) {
+
+    }
+
+    @Override
+    public void updateObservation(AgentBasedModel model, double ti) {
+
+    }
 }
--- src/hgm/abmodel/behaviour/AbsBehaviour.java
+++ src/hgm/abmodel/behaviour/AbsBehaviour.java
@@ -1,6 +1,7 @@
 package hgm.abmodel.behaviour;
 
 /**
+ *
  * Created by TimeWz on 2017/6/16.
  */
 public class AbsBehaviour {
